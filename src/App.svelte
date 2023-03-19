05c9c354d1bbae9eca91dcb5ff41a7ed446cb430
ssebastianoo <sebastianogirotto05@gmail.com>
2023-03-19 14:39:47 +0100

    background grid

--- src/App.svelte
+++ src/App.svelte
@@ -47,9 +47,13 @@
         loaded = true;
 
         if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.register("/service-worker.js", {
-                scope: "/",
-            });
+            navigator.serviceWorker
+                .register("/service-worker.js", {
+                    scope: "/",
+                })
+                .then((registration) => {
+                    registration.update();
+                });
         }
     });
 
